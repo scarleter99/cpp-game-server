772517ffe0cf050f50c9035cc7df37501fa63729
태범 <ddino99@naver.com>
2023-03-30 17:37:23 +0900

    소켓 프로그래밍 추가

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,54 +1,71 @@
 #include "pch.h"
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 #include "ThreadManager.h"
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
-
-class Knight
-{
-public:
-	int32 _hp = rand() % 1000;
-};
-
-class Monster
-{
-public:
-	int64 _id = 0;
-};
 
 int main()
 {
-	Knight* knights[100];
+	// WinSock 초기화 (ws2_3.lib 초기화), 관련 정보를 wsaData에 기록 192.168.0.48
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData))
+		return 0;
 
-	for (int32 i = 0; i < 100; i++)
-		knights[i] = ObjectPool<Knight>::Pop();
-
-	for (int32 i = 0; i < 100; i++)
+	// ----- 소캣 생성 -----
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
 	{
-		ObjectPool<Knight>::Push(knights[i]);
-		knights[i] = nullptr;
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
 	}
 
-	shared_ptr<Knight> sptr = ObjectPool<Knight>::MakeShared();
-	shared_ptr<Knight> sptr2 = MakeShared<Knight>();
+	// ----- BInd -----
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 알아서 지정
+	serverAddr.sin_port = ::htons(7777);
 
-	for (int32 i = 0; i < 5; i++)
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 	{
-		GThreadManager->Launch([]()
-			{
-				while (true)
-				{
-					Knight* knight = xnew<Knight>();
-
-					cout << knight->_hp << endl;
-
-					this_thread::sleep_for(10ms);
-
-					xdelete(knight);
-				}
-			});
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
+	}
+	
+	// ----- Listen -----
+	if (::listen(listenSocket, 10) == SOCKET_ERROR) // 10 : BakcLog 대기 클라이언트 한도
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
 	}
 
-	GThreadManager->Join();
+	// ------ Accept -----
+	while (true)
+	{
+		SOCKADDR_IN clientAddr; // IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Assept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! IP = " << ipAddress << endl;
+
+		// TODO
+
+	}
+
+	::WSACleanup();
 }
