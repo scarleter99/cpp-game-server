65f583aaee223a32f71473b90ab234a275cedd66
태범 <ddino99@naver.com>
2023-05-11 18:03:02 +0900

    WSAEventSelect 모델 학습

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -53,70 +53,107 @@ int main()
 	cout << "Listen!" << endl;
 
 	// WSAEventSelect 모델
-	// 
+	// 소켓 관련 네트워크 이벤트를 이벤트 객체를 통해 감지
+	// 생성: WSACreateEvent: 메뉴얼 리셋, 논시그널
+	// 삭제: WSACloseEvent
+	// 소켓, 이벤트 연동: WSAEventSelect(socket, event, networkEvents); - 논블로킹 모드 전환
+	// 신호 상태 감지: WSAWaitForMultipleEvents: 64제한
+	// 구체적인 네트워크 이벤트 확인: WSAEnumNetworkEvents
+	// 이벤트: FD_ACCEPT(접속 클라 존재), FD_READ(수신 가능), FD_WRITE(송신 가능), 
+	//		FD_CLOSE(상대 접속 종료), FD_CONNECT(통신 연결 완료), FD_OOB
+	
+	// ACCEPT()가 리턴한 소켓은 listenSocket과 동인한 속성
+	// WSAWOULDBLOCK 에러 발생 가능
+	// 이벤트 발생 후 소켓 함수를 호출하지 않으면, 동일한 다음 이벤트가 발생하지 않음
 
+	
+
+	vector<WSAEVENT> wsaEvents;
 	vector<Session> sessions; // 현재 접속중인 Client 관리
 	sessions.reserve(100);
 
-	fd_set reads;
-	fd_set writes;
+	WSAEVENT listenEvent = ::WSACreateEvent();
+	wsaEvents.push_back(listenEvent);
+	sessions.push_back(Session{ listenSocket });
+	if (::WSAEventSelect(listenSocket, listenEvent, FD_ACCEPT | FD_CLOSE) == SOCKET_ERROR)
+		return 0;
 
 	while (true)
 	{
-		FD_ZERO(&reads);
-		FD_ZERO(&writes);
+		// cEvents : event 수
+		// *Events; event 객체들
+		// waitAll: 전부 기다리는지, 하나만 완료되도 되는지
+		// timeout
+		// Alertable: FALSE
+		// return : 완료된 첫번째 이벤트 idx
+		int32 index = ::WSAWaitForMultipleEvents(wsaEvents.size(), &wsaEvents[0], FALSE, WSA_INFINITE, FALSE);
+		if (index == WSA_WAIT_FAILED)
+			continue;
 
-		FD_SET(listenSocket, &reads);
+		index -= WSA_WAIT_EVENT_0;
 
-		// Read할 차례인지 Write할 차례인지 판단 후 소켓 등록
-		for (Session& s : sessions)
+		WSANETWORKEVENTS networkEvents;
+		// s: 소캣
+		// eventObject: 해당 이벤트 객체 핸들과 연동된 이벤트 객체를 논시그널화
+		// &networkEvent: 네트워크 이벤트 / 오류 정보 저장
+		if (::WSAEnumNetworkEvents(sessions[index].socket, wsaEvents[index], &networkEvents) == SOCKET_ERROR)
+			continue;
+
+		// Listener 소켓 체크
+		if (networkEvents.lNetworkEvents & FD_ACCEPT)
 		{
-			if (s.recvBytes <= s.sendBytes)
-				FD_SET(s.socket, &reads);
-			else
-				FD_SET(s.socket, &writes);
-		}
+			// 에러 체크
+			if (networkEvents.iErrorCode[FD_ACCEPT_BIT] != 0)
+				continue;
 
-		// Read/Write 가능한 상태인 소켓만 남기고 제거
-		int32 retVal = ::select(0, &reads, &writes, nullptr, nullptr);
-		if (retVal == SOCKET_ERROR)
-			break;
-
-		if (FD_ISSET(listenSocket, &reads))
-		{
 			SOCKADDR_IN clientAddr;
 			int32 addrLen = sizeof(clientAddr);
+
 			SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
 			if (clientSocket != INVALID_SOCKET)
 			{
 				cout << "Accept!" << endl;
 
+				WSAEVENT clientEvent = ::WSACreateEvent();
+				wsaEvents.push_back(clientEvent);
 				sessions.push_back(Session{ clientSocket });
+				if (::WSAEventSelect(clientSocket, clientEvent, FD_READ | FD_WRITE | FD_CLOSE) == SOCKET_ERROR)
+					return 0;
 			}
 		}
 
-		for (Session& s : sessions)
+		// Client 소켓 체크
+		if (networkEvents.lNetworkEvents & FD_READ || networkEvents.lNetworkEvents & FD_WRITE)
 		{
-			if (FD_ISSET(s.socket, &reads))
+			if ((networkEvents.lNetworkEvents & FD_READ) && (networkEvents.iErrorCode[FD_READ] != 0))
+				continue;
+
+			if ((networkEvents.lNetworkEvents & FD_WRITE) && (networkEvents.iErrorCode[FD_WRITE] != 0))
+				continue;
+
+			Session& s = sessions[index];
+
+			if (s.recvBytes == 0)
 			{
 				int32 recvLen = ::recv(s.socket, s.recvBuffer, BUFSIZE, 0);
-				if (recvLen <= 0)
+				if (recvLen == SOCKET_ERROR && ::WSAGetLastError() != WSAEWOULDBLOCK)
 				{
-					// sessions 제거
+					// TODO : sessions 제거
 					continue;
 				}
 
 				s.recvBytes = recvLen;
+				cout << "Recv Data = " << recvLen << endl;
 			}
 
-			if (FD_ISSET(s.socket, &writes))
+			if (s.recvBytes > s.sendBytes)
 			{
 				// 일부 데이터만 보내는 경우 상정
-				int32 sendLen = ::send(s.socket, &s.recvBuffer[s.sendBytes], 
+				int32 sendLen = ::send(s.socket, &s.recvBuffer[s.sendBytes],
 					s.recvBytes - s.sendBytes, 0);
-				if (sendLen == SOCKET_ERROR)
+				if (sendLen == SOCKET_ERROR && ::WSAGetLastError() != WSAEWOULDBLOCK)
 				{
-					// sessions 제거
+					// TODO : sessions 제거
 					continue;
 				}
 
@@ -126,10 +163,16 @@ int main()
 					s.recvBytes = 0;
 					s.sendBytes = 0;
 				}
+
+				cout << "Send Data = " << sendLen << endl;
 			}
 		}
-	}
 
+		if (networkEvents.lNetworkEvents & FD_CLOSE)
+		{
+			// TODO : sessions 제거
+		}
+	}
 
 	::WSACleanup();
 }
